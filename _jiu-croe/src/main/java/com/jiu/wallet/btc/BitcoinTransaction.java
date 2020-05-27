f18c519214233c3748affc8d62768072a851d650
248323482 <248323482@qq.com>
2020-05-27 22:39:15 +0800

    jiu

--- _jiu-croe/src/main/java/com/jiu/wallet/btc/BitcoinTransaction.java
+++ _jiu-croe/src/main/java/com/jiu/wallet/btc/BitcoinTransaction.java
@@ -56,6 +56,7 @@ public class BitcoinTransaction {
     private NetworkParameters network;
 
     private List<BigInteger> prvKeys;
+    private BigInteger prvKey;
     // 2730 sat
     private final static long DUST_THRESHOLD = 2730;
 
@@ -76,12 +77,17 @@ public class BitcoinTransaction {
         private long sequence = 4294967295L;
     }
 
-    public void signTransaction(String chainID, BigInteger prvKey) throws Exception {
+    /**
+     *离线非隐性离线签名 转出到一个地址
+     * set prvKey
+     * @throws Exception
+     */
+    public void signTransaction() throws Exception {
         Transaction tran = new Transaction(network);
         //可交易金額
         long totalAmount = 0;
         for (UTXO output : getOutputs()) {
-            if (totalAmount >= (amount + fee)) {
+            if (amount!=-1L&&totalAmount >= (amount + fee)) {
                 //余额足够,跳出累加
                 break;
             }
@@ -93,6 +99,10 @@ public class BitcoinTransaction {
         //转出
         tran.addOutput(Coin.valueOf(getAmount()), Address.fromBase58(network, getTo()));
         //找零金额
+        if(amount==-1L){
+            //全部转出
+            amount =totalAmount-getFee();
+        }
         long changeAmount = totalAmount - (getAmount() + getFee());
         if (changeAmount >= DUST_THRESHOLD) {
             //转入到找零地址
@@ -104,12 +114,11 @@ public class BitcoinTransaction {
         }
         for (int i = 0; i < getOutputs().size(); i++) {
             UTXO output = getOutputs().get(i);
-            BigInteger privateKey = prvKey;
             ECKey ecKey;
-            if (output.getAddress().equals(ECKey.fromPrivate(privateKey).toAddress(network).toBase58())) {
-                ecKey = ECKey.fromPrivate(privateKey);
-            } else if (output.getAddress().equals(ECKey.fromPrivate(privateKey, false).toAddress(network).toBase58())) {
-                ecKey = ECKey.fromPrivate(privateKey, false);
+            if (output.getAddress().equals(ECKey.fromPrivate(prvKey).toAddress(network).toBase58())) {
+                ecKey = ECKey.fromPrivate(prvKey);
+            } else if (output.getAddress().equals(ECKey.fromPrivate(prvKey, false).toAddress(network).toBase58())) {
+                ecKey = ECKey.fromPrivate(prvKey, false);
             } else {
                 throw new Exception("otxo验证失败");
             }
@@ -135,8 +144,92 @@ public class BitcoinTransaction {
         log.info("txId : " + txHash);
     }
 
+    /**
+     * 多个私钥转出到一个地址
+     * set prvKeys
+     * @throws Exception
+     */
+    public void signTransactions() throws Exception {
+        Transaction tran = new Transaction(network);
+        //多个私钥
+        List<BigInteger> prvKeys = this.prvKeys;
+        //总金额
+        long totalAmount = 0;
+        for (UTXO output : getOutputs()) {
+            //-1表示转掉全部
+            if (amount!=-1L&&totalAmount >= (amount + fee)) {
+                //余额足够,跳出累加
+                break;
+            }
+            totalAmount += output.getAmount();
+        }
+        if (totalAmount < getAmount()) {
+            throw new RuntimeException("余额不足");
+        }
 
-    public void signSegWitTransaction(String chainId, String password) throws Exception {
+        if(amount==-1L){
+            //全部转出
+            amount =totalAmount-getFee();
+        }
+        //转出
+        tran.addOutput(Coin.valueOf(getAmount()), Address.fromBase58(network, getTo()));
+        //找零金额
+        long changeAmount = totalAmount - (getAmount() + getFee());
+        if (changeAmount >= DUST_THRESHOLD) {
+            //转入到找零地址
+            tran.addOutput(Coin.valueOf(changeAmount), changeAddress);
+        }
+        for (UTXO output : getOutputs()) {
+            //添加input
+            tran.addInput(Sha256Hash.wrap(output.getTxHash()), output.getVout(), new Script(NumericUtil.hexToBytes(output.getScriptPubKey())));
+        }
+        for (int i = 0; i < getOutputs().size(); i++) {
+            UTXO output = getOutputs().get(i);
+            ECKey ecKey=null;
+            for (BigInteger prvKey : prvKeys) {
+                try {
+                    if (output.getAddress().equals(ECKey.fromPrivate(prvKey).toAddress(network).toBase58())) {
+                        ecKey = ECKey.fromPrivate(prvKey);
+                        break;
+                    } else if (output.getAddress().equals(ECKey.fromPrivate(prvKey, false).toAddress(network).toBase58())) {
+                        ecKey = ECKey.fromPrivate(prvKey, false);
+                        break;
+                    }
+                }catch (Exception e){
+
+                }
+            }
+            if(ecKey!=null) {
+                TransactionInput transactionInput = tran.getInput(i);
+                Script scriptPubKey = ScriptBuilder.createOutputScript(Address.fromBase58(network, output.getAddress()));
+                Sha256Hash hash = tran.hashForSignature(i, scriptPubKey, Transaction.SigHash.ALL, false);
+                ECKey.ECDSASignature ecSig = ecKey.sign(hash);
+                TransactionSignature txSig = new TransactionSignature(ecSig, Transaction.SigHash.ALL, false);
+                if (scriptPubKey.isSentToRawPubKey()) {
+                    transactionInput.setScriptSig(ScriptBuilder.createInputScript(txSig));
+                } else {
+                    if (!scriptPubKey.isSentToAddress()) {
+                        throw new Exception("UNSUPPORT_SEND_TARGET");
+                    }
+                    transactionInput.setScriptSig(ScriptBuilder.createInputScript(txSig, ecKey));
+                }
+            }else{
+                throw new Exception("密钥解析失败");
+            }
+        }
+        //将signedHex  广播到链上
+        String signedHex = NumericUtil.bytesToHex(tran.bitcoinSerialize());
+        log.info("sgin : " + signedHex);
+        //交易ID
+        String txHash = NumericUtil.beBigEndianHex(Hash.sha256(Hash.sha256(signedHex)));
+        log.info("txId : " + txHash);
+    }
+
+    /**
+     * 隐性地址交易
+     * @throws Exception
+     */
+    public void signSegWitTransaction() throws Exception {
         long totalAmount = 0L;
         boolean hasChange = false;
         for (UTXO output : getOutputs()) {
@@ -153,6 +246,7 @@ public class BitcoinTransaction {
         } else {
             targetScriptPubKey = ScriptBuilder.createOutputScript(toAddress).getProgram();
         }
+        //找零地址
         byte[] changeScriptPubKey = ScriptBuilder.createP2SHOutputScript(changeAddress.getHash160()).getProgram();
         byte[] hashPrevouts;
         byte[] hashOutputs;
@@ -192,8 +286,6 @@ public class BitcoinTransaction {
             List<String> redeemScripts = new ArrayList<>();
             for (int i = 0; i < getOutputs().size(); i++) {
                 UTXO utxo = getOutputs().get(i);
-                // Metadata.FROM_WIF.equals(wallet.getMetadata().getSource())
-                BigInteger prvKey = true ? prvKeys.get(0) : prvKeys.get(i);
                 ECKey key = ECKey.fromPrivate(prvKey, true);
                 String redeemScript = String.format("0014%s", NumericUtil.bytesToHex(key.getPubKeyHash()));
                 redeemScripts.add(redeemScript);
@@ -265,7 +357,6 @@ public class BitcoinTransaction {
                 if (idx == 0) {
                     for (int i = 0; i < witnesses.size(); i++) {
                         //Metadata.FROM_WIF.equals(wallet.getMetadata().getSource())
-                        BigInteger prvKey = true ? prvKeys.get(0) : prvKeys.get(i);
                         ECKey ecKey = ECKey.fromPrivate(prvKey);
                         byte[] wit = witnesses.get(i);
                         stream.write(new VarInt(2).encode());
@@ -279,10 +370,180 @@ public class BitcoinTransaction {
             }
             byte[] signed = serialStreams[0].toByteArray();
             String signedHex = NumericUtil.bytesToHex(signed);
+            log.info("sgin : " + signedHex);
             String wtxID = NumericUtil.bytesToHex(Sha256Hash.hashTwice(signed));
             wtxID = NumericUtil.beBigEndianHex(wtxID);
             String txHash = NumericUtil.bytesToHex(Sha256Hash.hashTwice(serialStreams[1].toByteArray()));
             txHash = NumericUtil.beBigEndianHex(txHash);
+            log.info("txId : " + txHash);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+
+
+    public void signSegWitTransactions() throws Exception {
+        long totalAmount = 0L;
+        boolean hasChange = false;
+        for (UTXO output : getOutputs()) {
+            totalAmount += output.getAmount();
+        }
+        if (totalAmount < getAmount()) {
+            throw new Exception("余额不够");
+        }
+        long changeAmount = totalAmount - (getAmount() + getFee());
+        Address toAddress = Address.fromBase58(network, to);
+        byte[] targetScriptPubKey;
+        if (toAddress.isP2SHAddress()) {
+            targetScriptPubKey = ScriptBuilder.createP2SHOutputScript(toAddress.getHash160()).getProgram();
+        } else {
+            targetScriptPubKey = ScriptBuilder.createOutputScript(toAddress).getProgram();
+        }
+        //找零地址
+        byte[] changeScriptPubKey = ScriptBuilder.createP2SHOutputScript(changeAddress.getHash160()).getProgram();
+        byte[] hashPrevouts;
+        byte[] hashOutputs;
+        byte[] hashSequence;
+        try {
+            // calc hash prevouts
+            UnsafeByteArrayOutputStream stream = new UnsafeByteArrayOutputStream();
+            for (UTXO utxo : getOutputs()) {
+                TransactionOutPoint outPoint = new TransactionOutPoint(this.network, utxo.vout, Sha256Hash.wrap(utxo.txHash));
+                outPoint.bitcoinSerialize(stream);
+            }
+            hashPrevouts = Sha256Hash.hashTwice(stream.toByteArray());
+            // calc hash outputs
+            stream = new UnsafeByteArrayOutputStream();
+            TransactionOutput targetOutput = new TransactionOutput(this.network, null, Coin.valueOf(amount), toAddress);
+            targetOutput.bitcoinSerialize(stream);
+            if (changeAmount >= DUST_THRESHOLD) {
+                hasChange = true;
+                TransactionOutput changeOutput = new TransactionOutput(this.network, null, Coin.valueOf(changeAmount), changeAddress);
+                changeOutput.bitcoinSerialize(stream);
+            }
+            Utils.uint64ToByteStreamLE(BigInteger.valueOf(amount), stream);
+            stream.write(new VarInt(targetScriptPubKey.length).encode());
+            stream.write(targetScriptPubKey);
+            Utils.uint64ToByteStreamLE(BigInteger.valueOf(changeAmount), stream);
+            stream.write(new VarInt(changeScriptPubKey.length).encode());
+            stream.write(changeScriptPubKey);
+            hashOutputs = Sha256Hash.hashTwice(stream.toByteArray());
+            // calc hash sequence
+            stream = new UnsafeByteArrayOutputStream();
+            for (UTXO utxo : getOutputs()) {
+                Utils.uint32ToByteStreamLE(utxo.getSequence(), stream);
+            }
+            hashSequence = Sha256Hash.hashTwice(stream.toByteArray());
+            // calc witnesses and redemScripts
+            List<byte[]> witnesses = new ArrayList<>();
+            List<String> redeemScripts = new ArrayList<>();
+            for (int i = 0; i < getOutputs().size(); i++) {
+                UTXO utxo = getOutputs().get(i);
+                ECKey key =null;
+                List<BigInteger> prvKeys = this.prvKeys;
+                for (BigInteger prvKey:prvKeys) {
+                    try {
+                        key = ECKey.fromPrivate(prvKey, true);
+                    }catch (Exception e){
+
+                    }
+
+                }
+                if(key!=null) {
+                    String redeemScript = String.format("0014%s", NumericUtil.bytesToHex(key.getPubKeyHash()));
+                    redeemScripts.add(redeemScript);
+                    // calc outpoint
+                    stream = new UnsafeByteArrayOutputStream();
+                    TransactionOutPoint txOutPoint = new TransactionOutPoint(this.network, utxo.vout, Sha256Hash.wrap(utxo.txHash));
+                    txOutPoint.bitcoinSerialize(stream);
+                    byte[] outpoint = stream.toByteArray();
+                    // calc scriptCode
+                    byte[] scriptCode = NumericUtil.hexToBytes(String.format("0x1976a914%s88ac", NumericUtil.bytesToHex(key.getPubKeyHash())));
+                    // before sign
+                    stream = new UnsafeByteArrayOutputStream();
+                    Utils.uint32ToByteStreamLE(2L, stream);
+                    stream.write(hashPrevouts);
+                    stream.write(hashSequence);
+                    stream.write(outpoint);
+                    stream.write(scriptCode);
+                    Utils.uint64ToByteStreamLE(BigInteger.valueOf(utxo.getAmount()), stream);
+                    Utils.uint32ToByteStreamLE(utxo.getSequence(), stream);
+                    stream.write(hashOutputs);
+                    Utils.uint32ToByteStreamLE(locktime, stream);
+                    // hashType 1 = all
+                    Utils.uint32ToByteStreamLE(1L, stream);
+                    byte[] hashPreimage = stream.toByteArray();
+                    byte[] sigHash = Sha256Hash.hashTwice(hashPreimage);
+                    ECKey.ECDSASignature signature = key.sign(Sha256Hash.wrap(sigHash));
+                    byte hashType = 0x01;
+                    // witnesses
+                    byte[] sig = ByteUtil.concat(signature.encodeToDER(), new byte[]{hashType});
+                    witnesses.add(sig);
+                }else{
+                    throw  new Exception("签名失败");
+                }
+            }
+            // the second stream is used to calc the traditional txhash
+            UnsafeByteArrayOutputStream[] serialStreams = new UnsafeByteArrayOutputStream[]{
+                    new UnsafeByteArrayOutputStream(), new UnsafeByteArrayOutputStream()
+            };
+            for (int idx = 0; idx < 2; idx++) {
+                stream = serialStreams[idx];
+                Utils.uint32ToByteStreamLE(2L, stream); // version
+                if (idx == 0) {
+                    stream.write(0x00); // maker
+                    stream.write(0x01); // flag
+                }
+                // inputs
+                stream.write(new VarInt(getOutputs().size()).encode());
+                for (int i = 0; i < getOutputs().size(); i++) {
+                    UTXO utxo = getOutputs().get(i);
+                    stream.write(NumericUtil.reverseBytes(NumericUtil.hexToBytes(utxo.txHash)));
+                    Utils.uint32ToByteStreamLE(utxo.getVout(), stream);
+                    // the length of byte array that follows, and this length is used by OP_PUSHDATA1
+                    stream.write(0x17);
+                    // the length of byte array that follows, and this length is used by cutting array
+                    stream.write(0x16);
+                    stream.write(NumericUtil.hexToBytes(redeemScripts.get(i)));
+                    Utils.uint32ToByteStreamLE(utxo.getSequence(), stream);
+                }
+                // outputs
+                // outputs size
+                int outputSize = hasChange ? 2 : 1;
+                stream.write(new VarInt(outputSize).encode());
+                Utils.uint64ToByteStreamLE(BigInteger.valueOf(amount), stream);
+                stream.write(new VarInt(targetScriptPubKey.length).encode());
+                stream.write(targetScriptPubKey);
+                if (hasChange) {
+                    Utils.uint64ToByteStreamLE(BigInteger.valueOf(changeAmount), stream);
+                    stream.write(new VarInt(changeScriptPubKey.length).encode());
+                    stream.write(changeScriptPubKey);
+                }
+                // the first stream is used to calc the segwit hash
+                if (idx == 0) {
+                    for (int i = 0; i < witnesses.size(); i++) {
+                        //Metadata.FROM_WIF.equals(wallet.getMetadata().getSource())
+                        ECKey ecKey = ECKey.fromPrivate(prvKey);
+                        byte[] wit = witnesses.get(i);
+                        stream.write(new VarInt(2).encode());
+                        stream.write(new VarInt(wit.length).encode());
+                        stream.write(wit);
+                        stream.write(new VarInt(ecKey.getPubKey().length).encode());
+                        stream.write(ecKey.getPubKey());
+                    }
+                }
+                Utils.uint32ToByteStreamLE(locktime, stream);
+            }
+            byte[] signed = serialStreams[0].toByteArray();
+            String signedHex = NumericUtil.bytesToHex(signed);
+            log.info("sgin : " + signedHex);
+            String wtxID = NumericUtil.bytesToHex(Sha256Hash.hashTwice(signed));
+            wtxID = NumericUtil.beBigEndianHex(wtxID);
+            String txHash = NumericUtil.bytesToHex(Sha256Hash.hashTwice(serialStreams[1].toByteArray()));
+            txHash = NumericUtil.beBigEndianHex(txHash);
+            log.info("txId : " + txHash);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -377,8 +638,7 @@ public class BitcoinTransaction {
         return dumpedPrivateKey.getKey();
     }
     public static Long getFee(long amount, List<UTXO> utxos) {
-        String str = HttpUtil.createGet("https://bitcoinfees.earn.com/api/v1/fees/recommended").execute().body();
-        Long feeRate = ((JSONObject)JSONObject.parseObject(str)).getLong("fastestFee"); //最新的矿工费率
+        Long feeRate =40L; //((JSONObject)JSONObject.parseObject(HttpUtil.createGet("https://bitcoinfees.earn.com/api/v1/fees/recommended").execute().body())).getLong("fastestFee"); //最新的矿工费率
         Long utxoAmount = 0L;
         Long fee = 0L;
         Long utxoSize = 0L;
@@ -388,7 +648,7 @@ public class BitcoinTransaction {
                 break;
             } else {
                 utxoAmount += us.getAmount();
-                fee = (utxoSize * 148 * 34  + 10) * feeRate;
+                fee = (utxoSize * 148 + 34  + 10) * feeRate;
             }
         }
         return fee;
