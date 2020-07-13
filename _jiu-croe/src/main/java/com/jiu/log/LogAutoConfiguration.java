8a6d74865073644acb4ea371162654bf59ce33ad
248323482 <248323482@qq.com>
2020-07-13 18:56:06 +0800

    添加日志

--- _jiu-croe/src/main/java/com/jiu/log/LogAutoConfiguration.java
+++ _jiu-croe/src/main/java/com/jiu/log/LogAutoConfiguration.java
@@ -7,6 +7,8 @@ import com.jiu.log.event.SysLogListener;
 import com.jiu.log.interceptor.MdcMvcConfigurer;
 import com.jiu.log.monitor.PointUtil;
 import com.jiu.log.properties.OptLogProperties;
+import com.plumelog.core.TraceId;
+import com.plumelog.core.util.IdWorker;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -15,6 +17,11 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplicat
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 日志自动配置
@@ -28,6 +35,17 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @ConditionalOnWebApplication
 @ConditionalOnProperty(prefix = OptLogProperties.PREFIX, name = "enabled", havingValue = "true", matchIfMissing = true)
 public class LogAutoConfiguration {
+
+    // IdWorker
+    @Component
+    public class Interceptor extends HandlerInterceptorAdapter {
+        private IdWorker worker = new IdWorker(1,1,1);//雪花算法，这边不一定要用这个生成id
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            TraceId.logTraceID.set(String.valueOf(worker.nextId()));//设置TraceID值，不埋此点链路ID就没有
+            return true;
+        }
+    }
 
     @Bean
     @ConditionalOnMissingBean
